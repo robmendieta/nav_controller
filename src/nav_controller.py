780118642519e6143b02d0781e5cf0f67a3f8391
robmendieta <rmendietalopez@gmail.com>
2016-03-21 12:50:21 +0100

    empty node file

---
+++ src/nav_controller.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+This script is a simple python node which imports
+source code of common_messages_examples.
+"""
+#-*- encoding: utf-8 -*-
+__author__ = 'roberto_mendieta'
+
+import rospy
+
+#State machine
+#Node init
+#Logic
+
+if __name__ == '__main__':
+    
