260328cccf05435d89ae8571f59a2f8e9a9a4c40
robmendieta <rmendietalopez@gmail.com>
2016-03-22 23:36:56 +0100

    Mobility added, robot goes to point relative to odom and stops

--- scripts/nav_controller.py
+++ scripts/nav_controller.py
@@ -18,22 +18,26 @@ from tf2_msgs.msg import *
 
 class NavClass():
     def __init__(self):
-	   #Listener
-        self.parent_tfFrame = 'base_footprint'
-        self.child_tfFrame = 'odom'
-        self.tfBuffer = tf2_ros.Buffer()
-        self.listener = tf2_ros.TransformListener(self.tfBuffer)
-        self.tfx = 0.0
-        self.tfy = 0.0
-        
-        #Odometry
+        #Pose 
+        self.pose_msg = PoseStamped()
+        #Create Pose transform
+        self.pose_transform = geometry_msgs.msg.TransformStamped()
+        #Pose transform broadcaster
+        self.pub_tf = tf2_ros.TransformBroadcaster()       
+
+        #Odometry msg init
         self.odometry_msg = Odometry()
         self.originx = 0.0
         self.originy = 0.0
         
-        #Pose 
-        self.pose_msg = PoseStamped()
-        
+	   #Transform listener
+        self.parent_tfFrame = 'odom'
+        self.child_tfFrame = 'base_footprint'
+        self.tfBuffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tfBuffer)
+        self.tfx = 0.0
+        self.tfy = 0.0
+                
         #Publish speed
         self.speed_pub = rospy.Publisher('cmd_vel', geometry_msgs.msg.Twist, queue_size=1)
         self.speed_msg = geometry_msgs.msg.Twist()
@@ -42,55 +46,95 @@ class NavClass():
         self.odometry_msg = msg
         self.originx = self.odometry_msg.pose.pose.position.x
         self.originy = self.odometry_msg.pose.pose.position.y
-
-        #TF Listener
-        self.getTF()
          
     def getTF(self):
-        try:
-            trans = self.tfBuffer.lookup_transform(self.parent_tfFrame,self.child_tfFrame, rospy.Time())
-            self.tfx = trans.transform.translation.x
-            self.tfy = trans.transform.translation.y
-        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-            rate.sleep()
-
+        #Fill pose_transform with data from pose_msg
+        self.pose_transform.transform.translation.x = self.pose_msg.pose.position.x
+        self.pose_transform.transform.translation.y = self.pose_msg.pose.position.y
+        self.pose_transform.transform.translation.z = self.pose_msg.pose.position.z
         
+        self.pose_transform.transform.rotation.x = self.pose_msg.pose.orientation.x
+        self.pose_transform.transform.rotation.y = self.pose_msg.pose.orientation.y
+        self.pose_transform.transform.rotation.z = self.pose_msg.pose.orientation.z
+        self.pose_transform.transform.rotation.w = self.pose_msg.pose.orientation.w
+        
+        self.pose_transform.header.frame_id = "odom"
+        self.pose_transform.header.stamp = rospy.Time.now()
+        self.pose_transform.child_frame_id = "pose_frame"
+        
+        #Send transform
+        self.pub_tf.sendTransform(self.pose_transform)
+        
+        #TF listener, can llok for transforms
+        #Should implement for flexibility and frame migration
+        rate = rospy.Rate(10.0)
+        try:
+            self.trans = self.tfBuffer.lookup_transform(self.parent_tfFrame,self.child_tfFrame, rospy.Time())
+            self.tfx = self.trans.transform.translation.x
+            self.tfy = self.trans.transform.translation.y
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            rospy.loginfo("EXCEPTION!!!!!!")
+            rate.sleep()
+   
     def runRobot(self):
         rospy.loginfo("Running the robot")
         
-        #TF        
-        rospy.loginfo("TF Listener parent: %s , child: %s ",self.parent_tfFrame,self.child_tfFrame)    
-        rospy.loginfo("TF Listener position: x = %f , y = %f ",self.tfx,self.tfy)
+        rate = rospy.Rate(10.0)
         
-        #Odom
-        rospy.loginfo("nav_class origin frame_ID:" + self.odometry_msg.header.frame_id)	
-        rospy.loginfo("nav_class child_frame_ID: %s" , self.odometry_msg.child_frame_id) 
-        rospy.loginfo("position: x = %f , y = %f ",self.originx,self.originy)	
+        #Get distance
+        distancex = self.pose_msg.pose.position.x - self.originx
+        distancey = self.pose_msg.pose.position.y - self.originy
+        rospy.loginfo("Distance x: %f" , distancex)
+        rospy.loginfo("Distance y: %f" , distancey)
         
-        #Pose
-        rospy.loginfo("nav_class pose frame_ID:" + self.pose_msg.header.frame_id)	
-        rospy.loginfo("position: x = %f , y = %f ",self.pose_msg.pose.position.x,self.pose_msg.pose.position.y)
-        xgoal = self.pose_msg.pose.position.x
-        ygoal = self.pose_msg.pose.position.y
+        while (distancex > 0.01 or distancex < -0.01) and (distancey > 0.01 or distancey < -0.01):
+            #TF listener values (only updated at start)
+            rospy.loginfo("TF Listener parent:" + self.trans.header.frame_id + " child: " + self.trans.child_frame_id)    
+            rospy.loginfo("TF Listener position: x = %f , y = %f " , self.tfx , self.tfy)
         
-        while xgoal-self.originx != 0 and ygoal-self.originy != 0:
-            self.speed_msg.linear.x = xgoal-self.originx*0.1
-            self.speed_msg.linear.y = ygoal-self.originy*0.1
+            #Odom (updated each loop)
+            rospy.loginfo("ODOM position: x = %f , y = %f ",self.originx , self.originy)	
+        
+            #Pose (from pose_msg)
+            rospy.loginfo("nav_class pose frame_ID:" + self.pose_msg.header.frame_id)	
+            rospy.loginfo("position: x = %f , y = %f ",self.pose_msg.pose.position.x , self.pose_msg.pose.position.y)
+            rate.sleep()
+            
+            distancex = self.pose_msg.pose.position.x-self.originx
+            distancey = self.pose_msg.pose.position.y-self.originy
+            rospy.loginfo("Distance x: %f" , distancex)
+            rospy.loginfo("Distance y: %f" , distancey)
+            
+            #All other speeds to 0
+            self.speed_msg.angular.x = 0.0
+            self.speed_msg.angular.y = 0.0
+            self.speed_msg.angular.z = 0.0
+            self.speed_msg.linear.x = distancex
+            self.speed_msg.linear.y = distancey
+            self.speed_msg.linear.z = 0.0
             self.speed_pub.publish(self.speed_msg)
-    
+            rate.sleep()
+            
+        rospy.loginfo("Distance x: %f" , distancex)
+        rospy.loginfo("Distance y: %f" , distancey)    
+        self.stopRobot()
+        
+   
     def stopRobot(self):
         rospy.loginfo("Stopping the robot")
-        self.speed_msg.linear.x = 0
-        self.speed_msg.angular.z = 0
+        self.speed_msg.linear.x = 0.0
+        self.speed_msg.linear.y = 0.0
         self.speed_pub.publish(self.speed_msg)
 
 			       
     def setOdom(self,msg):
-        nav_class.getOrigin(msg)
+        self.getOrigin(msg)
 	    
     def setPose(self,msg):
-        rospy.loginfo("Subscribing to pose")
         self.pose_msg = msg
+        rospy.loginfo("Subscribing to pose")
+        #TF Listener
+        self.getTF()    
         
         
 
@@ -113,7 +157,6 @@ if __name__ == '__main__':
     #Subscribe to odom
     rospy.Subscriber("~/relative_pose", PoseStamped, nav_class.setPose)
     rospy.Subscriber("odom", Odometry, nav_class.setOdom)
-    #rospy.Subscriber("tf",TFMessage,tf_cb)
     rospy.Subscriber("~/event_in", String, nav_class.checkEventMsg)
     rospy.spin()
     
